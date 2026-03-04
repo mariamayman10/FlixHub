d32031d0ed50c2b7dfbb77e33cf0c691292d2e9f
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-03-04 23:14:48 +0200

    optimize performance

--- src/Components/AddWatchList.js
+++ src/Components/AddWatchList.js
@@ -19,7 +19,7 @@ const AddWatchList = () => {
         style={{ verticalAlign: "middle" }}
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
         />
       </svg>
